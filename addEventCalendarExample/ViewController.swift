d6180bc156bccaa53010315967d8f35fa7bebfa1
asketola <annemarie.s.ketola@gmail.com>
2015-08-16 15:43:43 -0700

    Cleaned up the code

--- addEventCalendarExample/ViewController.swift
+++ addEventCalendarExample/ViewController.swift
@@ -12,23 +12,18 @@ import EventKit
 var dish = "entree"
 var doesCalendarExist : Bool = false
 var cookingClubCalendar : EKCalendar?
-//var newCookingClubCalendar : EKCalendar?
 
-
-// 1 An EKEventStore object is created. This represents the Calendar's database
+// An EKEventStore object is created. This represents the Calendar's database
 let eventStore = EKEventStore()
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        // 1 An EKEventStore object is created. This represents the Calendar's database
-//        let eventStore = EKEventStore()
         
-        // 2 The authorizationStatusForEntityType method returns the authorization status
+        // The authorizationStatusForEntityType method returns the authorization status
         switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
         case .Authorized:
-//            insertEvent(eventStore)
             println("We are authorized")
         case .Denied:
             println("Access denied")
@@ -52,12 +47,11 @@ class ViewController: UIViewController {
     
     
 func insertEvent(store: EKEventStore) {
-    // 1 The calendarsForEntityType returns all calendars that supports events
+    // The calendarsForEntityType returns all calendars that supports events
     let calendars = store.calendarsForEntityType(EKEntityTypeEvent)
             as! [EKCalendar]
-//    println("All the calendars we have are: \(calendars)")
     
-    // 2 We check if the previously created calendar "Cooking Club" exists
+    // We check if the previously created calendar "Cooking Club" exists
     for calendar in calendars {
         if calendar.title == "Cooking Club" {
             cookingClubCalendar = calendar
@@ -72,8 +66,7 @@ func insertEvent(store: EKEventStore) {
         // 2 hours standard cooking club time (?) can move this to 3
             let endDate = startDate.dateByAddingTimeInterval(2 * 60 * 60)
                 
-        // 4 An event is created with a title of "New meeting"
-        // Create Event
+        // An event is created with a title of CookingClub's name and the dish you are bringing
         var event = EKEvent(eventStore: store)
             event.calendar = cookingClubCalendar
                 
@@ -84,8 +77,7 @@ func insertEvent(store: EKEventStore) {
             event.endDate = endDate
             println("endDate = \(endDate)")
                 
-        // 5 The event is saved into the current calendar.
-        // Save Event in Calendar
+        // 5 The event is saved into the current calendar
         var error: NSError?
         let result = store.saveEvent(event, span: EKSpanThisEvent, error: &error)
                 
@@ -94,12 +86,7 @@ func insertEvent(store: EKEventStore) {
                 println("An error occured \(theError)")
                 }
             } else {
-            println("We created an event through the if calendar.title == Cooking Club")
-//            self.performSegueWithIdentifier("SHOW_DONE", sender: self)
-            let successAlert = UIAlertController(title: "Success", message: "Your event has been saved", preferredStyle: .Alert)
-            let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
-            successAlert.addAction(OKAction)
-            self.presentViewController(successAlert, animated: true, completion: nil)
+            successAlertController()
             }
         } else {
             // create the new calendar
@@ -120,17 +107,13 @@ func insertEvent(store: EKEventStore) {
             } else {
                 println("The newCalendar was saved")
                 NSUserDefaults.standardUserDefaults().setObject(newCalendar.calendarIdentifier, forKey: "addEventCalendarExample")
-                // then add an event
                 // The event has a start date of the current time and an end date of 2 hours from the current time. (2 hours x 60 minutes x 60 seconds)
                 let startDate = NSDate()
-                // 2 hours
                 let endDate = startDate.dateByAddingTimeInterval(2 * 60 * 60)
                 
-                // 4 An event is created with a title of "New meeting"
-                // Create Event
+                // An event is created with a title of CookingClub's name and the dish you are bringing
                 var event = EKEvent(eventStore: store)
                 event.calendar = newCalendar
-                
                 event.title = "Cooking Club: You have \(dish)"
                 
                 event.startDate = startDate
@@ -138,8 +121,7 @@ func insertEvent(store: EKEventStore) {
                 event.endDate = endDate
                 println("endDate = \(endDate)")
                 
-                // 5 The event is saved into the current calendar.
-                // Save Event in Calendar
+                // The event is saved into the current calendar
                 var error: NSError?
                 let result = store.saveEvent(event, span: EKSpanThisEvent, error: &error)
                 
@@ -148,16 +130,20 @@ func insertEvent(store: EKEventStore) {
                         println("An error occured \(theError)")
                     }
                 } else {
-                    println("We saved the event")
-//                    self.performSegueWithIdentifier("SHOW_DONE", sender: self)
-                    let successAlert = UIAlertController(title: "Success", message: "Your event has been saved", preferredStyle: .Alert)
-                    let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
-                    successAlert.addAction(OKAction)
-                    self.presentViewController(successAlert, animated: true, completion: nil)
+                    successAlertController()
                 }
             }
         }
       }
+    
+    func successAlertController() {
+        println("We saved the event")
+        //                    self.performSegueWithIdentifier("SHOW_DONE", sender: self)
+        let successAlert = UIAlertController(title: "Success", message: "Your event has been saved", preferredStyle: .Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        successAlert.addAction(OKAction)
+        self.presentViewController(successAlert, animated: true, completion: nil)
+    }
     
     
     @IBAction func setReminderButtonPressed(sender: AnyObject) {
