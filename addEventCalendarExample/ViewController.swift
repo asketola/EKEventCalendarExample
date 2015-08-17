2697a48b4550717f28806d8647d55b6548511765
asketola <annemarie.s.ketola@gmail.com>
2015-08-16 23:02:21 -0700

    Added Alert to the Calendar reminder

--- addEventCalendarExample/ViewController.swift
+++ addEventCalendarExample/ViewController.swift
@@ -12,9 +12,12 @@ import EventKit
 var dish = "entree"
 var doesCalendarExist : Bool = false
 var cookingClubCalendar : EKCalendar?
+var startDate : NSDate!
+var alarmDate : NSDate!
 
 // An EKEventStore object is created. This represents the Calendar's database
 let eventStore = EKEventStore()
+let calendarDatabase = EKEventStore()
 
 class ViewController: UIViewController {
 
@@ -43,6 +46,20 @@ class ViewController: UIViewController {
             println("Case Default")
         }
         println("doesCalendarExist \(doesCalendarExist) ")
+        
+        // check for status for alarm
+        calendarDatabase.requestAccessToEntityType(EKEntityTypeReminder, completion: { (granted: Bool, error:NSError!) -> Void in
+            if !granted {
+                println("Access to store not granted")
+            }
+        })
+        
+        var dataString = "August 16, 2015 22:03"
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
+        dateFormatter.timeZone = NSTimeZone.localTimeZone()
+        startDate = dateFormatter.dateFromString(dataString) as NSDate!
+        alarmDate = startDate.dateByAddingTimeInterval(-60*60)
     }
     
     
@@ -61,15 +78,13 @@ func insertEvent(store: EKEventStore) {
     }
         
         if doesCalendarExist == true {
-        // 3 The event has a start date of the current time and an end date of 2 hours from the current time. (2 hours x 60 minutes x 60 seconds)
-            let startDate = NSDate()
+        // 3 The event has a start date of the current time and an end date of 2 hours from the current time. (2 hours x 60 minutes x 60 seconds
         // 2 hours standard cooking club time (?) can move this to 3
             let endDate = startDate.dateByAddingTimeInterval(2 * 60 * 60)
                 
         // An event is created with a title of CookingClub's name and the dish you are bringing
         var event = EKEvent(eventStore: store)
             event.calendar = cookingClubCalendar
-                
             event.title = "Cooking Club: You have \(dish)"
             
             event.startDate = startDate
@@ -87,6 +102,7 @@ func insertEvent(store: EKEventStore) {
                 }
             } else {
             successAlertController()
+            
             }
         } else {
             // create the new calendar
@@ -139,10 +155,60 @@ func insertEvent(store: EKEventStore) {
     func successAlertController() {
         println("We saved the event")
         //                    self.performSegueWithIdentifier("SHOW_DONE", sender: self)
-        let successAlert = UIAlertController(title: "Success", message: "Your event has been saved", preferredStyle: .Alert)
+        let successAlert = UIAlertController(title: "Success", message: "Your event has been added to your Calendar", preferredStyle: .Alert)
         let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
         successAlert.addAction(OKAction)
+        let AddAlarmAction = UIAlertAction(title: "Add alarm 1hr prior to event", style: .Default) { (AddAlarmAction) -> Void in
+            self.createReminder("reminderset", reminderDate: alarmDate)
+        }
+        successAlert.addAction(AddAlarmAction)
         self.presentViewController(successAlert, animated: true, completion: nil)
+    }
+    
+    func accessCalendarInTheDatabase() {
+        var calendars = calendarDatabase.calendarsForEntityType(EKEntityTypeReminder)
+        
+        for calendar in calendars as! [EKCalendar] {
+            println("Calendar = \(calendar.title)")
+        }
+    }
+    
+    func createReminder(reminderTitle: String, reminderDate: NSDate) {
+        let reminder = EKReminder(eventStore: calendarDatabase)
+        
+        reminder.title = "Cooking Club tonight! You have \(dish)"
+        let date = reminderDate
+        let alarm = EKAlarm(absoluteDate: date)
+        
+        reminder.addAlarm(alarm)
+        
+        reminder.calendar = calendarDatabase.defaultCalendarForNewReminders()
+        
+        var error = NSError?()
+        
+        if error != nil {
+            println("errors: \(error?.localizedDescription)")
+        }
+        
+        calendarDatabase.saveReminder(reminder, commit: true, error: &error)
+    }
+    
+    @IBAction func setReminderForAlarm(sender: AnyObject) {
+        
+        calendarDatabase.requestAccessToEntityType(EKEntityTypeReminder, completion: { (granted, error) -> Void in
+            if !granted {
+                println("Access to store not granted")
+                println("\(error.localizedDescription)")
+            } else {
+                println("We have permission to make an alarm")
+                println("Reminder entered: \(alarmDate)")
+            }
+            
+        })
+        
+        var alarmDate = startDate.dateByAddingTimeInterval(-60*60)
+        createReminder("reminderset", reminderDate: startDate)
+        
     }
     
     
@@ -152,7 +218,6 @@ func insertEvent(store: EKEventStore) {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
